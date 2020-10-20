8cfb79a33587109833235b5b259dea28b5aac26e
fitra <fitra.adinugraha@gmail.com>
2020-10-21 06:48:02 +0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const properties = require("properties-reader");
+
+function splitToJson(msg,str,i,length,value){
+    if(!msg[str[i]]){
+        msg[str[i]] = {};
+    }
+    if(i+1<length){
+        return splitToJson(msg[str[i]],str,i+1,length,value);
+    }else{
+        msg[str[i]] = value;
+        return;
+    }
+
+}
+
+
+
+module.exports = (str) => {
+    const message = {};
+    properties(str).each((key, value) => {
+        // called for each item in the reader,
+        // first with key=main.some.thing, value=foo
+        // next with key=blah.some.thing, value=bar
+        const l = key.split('.');
+        splitToJson(message,l,0,l.length,value)
+        //message[key]=value;
+    });
+    return message;
+}
